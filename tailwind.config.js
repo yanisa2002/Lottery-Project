d05c6a3b3eca8ceb30ada1ea11bad43daab3b1d7
YANISA PONGMETHA <yanisa2002@gmail.com>
2022-04-16 13:47:44 +0700

    update setting font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        prompt: "'Prompt', sans-serif",
+      },
+    },
   },
   plugins: [],
-}
+};
