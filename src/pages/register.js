c4108f51583d81f19f6c6afb4e6c3bf51a881ee3
YANISA PONGMETHA <yanisa2002@gmail.com>
2022-04-19 00:43:05 +0700

    update register

--- src/pages/register.js
+++ src/pages/register.js
@@ -4,15 +4,15 @@ const Register = () => {
   return (
     <div className="h-full flex justify-center  bg-[#FFE5A3] font-prompt">
       <div>
-        <div className="mt-6 text-3xl font-bold text-center text-red-600">
+        <div className="mt-6 text-3xl font-bold text-center text-[#E54E3D]">
           ยินดีต้อนรับ!
         </div>
         <div className="mt-2 text-xl font-medium text-center">
           กรอกข้อมูลด้านล่างเพื่อเข้าร่วมเป็นสมาชิก
         </div>
-        <div className="flex flex-col p-8 m-8 bg-white md:min-w-[600px] sm:min-w-[400px] min-w-[300px]  rounded-xl shadow-xl">
+        <div className="flex flex-col p-8 m-8 bg-white w-[600px] sm:min-w-[400px] min-w-[300px]  rounded-xl shadow-xl">
           <h1 className="text-xl font-bold mb-2">สร้างบัญชีของคุณ</h1>
-          <h2 className="text-lg mb-4 text-red-600">ข้อมูลส่วนตัว</h2>
+          <h2 className="text-lg mb-4 text-[#E54E3D]">ข้อมูลส่วนตัว</h2>
           <label
             className="block text-gray-darker text-md font-bold mb-2"
             for="prefix"
@@ -29,7 +29,7 @@ const Register = () => {
             <option value="Miss">นางสาว</option>
           </select>
 
-          <div className="flex flex-col grid grid-cols-2 gap-9">
+          <div className=" flex-col grid grid-cols-2 gap-9">
             <div className="flex flex-col">
               <label
                 className="block text-gray-darker text-md font-bold mt-4 mb-2"
@@ -152,61 +152,131 @@ const Register = () => {
             placeholder="Phone Number"
           ></input>
 
-          <h2 className="text-lg mt-8 mb-4 text-red-600">ที่อยู่</h2>
-          <div className="grid grid-cols-6 gap-6">
-            {/* <div>
-              <label
-                className="block text-gray-darker text-md font-bold mt-4 mb-2"
-                for="number"
-              >
-                บ้านเลขที่
+          <h2 className="text-lg mt-8 mb-4 text-[#E54E3D]">ที่อยู่</h2>
+          <div className="grid grid-cols-6 ">
+            <label
+              className="block text-gray-darker text-md font-bold mt-4 mb-2"
+              for="number"
+            >
+              บ้านเลขที่
+            </label>
+
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="number"
+              type="text"
+              placeholder="No."
+            ></input>
+
+            <label
+              className="block text-gray-darker text-md font-bold text-center mt-4 mb-2"
+              for="sol"
+            >
+              ซอย
+            </label>
+
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="sol"
+              type="text"
+              placeholder="Sol"
+            ></input>
+
+            <label
+              className="block text-gray-darker text-md font-bold text-center mt-4 mb-2"
+              for="road"
+            >
+              ถนน
+            </label>
+
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="road"
+              type="text"
+              placeholder="Road"
+            ></input>
+          </div>
+
+          <div className="grid grid-cols-4 mt-4 ">
+            <label
+              className="block text-gray-darker text-md font-bold mt-4 mb-2"
+              for="subdistrict"
+            >
+              แขวง/ตำบล
+            </label>
+
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="subdistrict"
+              type="text"
+              placeholder="Sub District"
+            ></input>
+
+            <label
+              className="block text-gray-darker text-md font-bold text-center mt-4 mb-2"
+              for="district"
+            >
+              เขต/อำเภอ
+            </label>
+
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="district"
+              type="text"
+              placeholder="District"
+            ></input>
+          </div>
+
+          <div className="grid grid-cols-4 mt-4 ">
+            <label
+              className="block text-gray-darker text-md font-bold mt-4 mb-2"
+              for="province"
+            >
+              จังหวัด
+            </label>
+
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="province"
+              type="text"
+              placeholder="Province"
+            ></input>
+
+            <label
+              className="block text-gray-darker text-md font-bold text-center mt-4 mb-2"
+              for="zipcode"
+            >
+              รหัสไปรษณีย์
+            </label>
+
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="zipcode"
+              type="text"
+              placeholder="Zip Code"
+            ></input>
+          </div>
+
+          <div className="block">
+            <div className="mt-5">
+              <label className="inline-flex items-center">
+                <input type="checkbox" class="w-6 h-6 rounded" checked />
+                <span className="text-gray-darker text-md font-bold ml-2">
+                  ต้องการเป็นผู้ขาย
+                </span>
               </label>
             </div>
+          </div>
 
-            <div>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                id="number"
-                type="text"
-                placeholder="No."
-              ></input>
-            </div>
-
-            <div>
-              <label
-                className="block text-gray-darker text-md font-bold mt-4 mb-2"
-                for="sol"
-              >
-                ซอย
-              </label>
-            </div>
-
-            <div>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                id="sol"
-                type="text"
-                placeholder="Sol"
-              ></input>
-            </div>
-
-            <div>
-              <label
-                className="block text-gray-darker text-md font-bold mt-4 mb-2"
-                for="road"
-              >
-                ถนน
-              </label>
-            </div>
-
-            <div>
-              <input
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-                id="road"
-                type="text"
-                placeholder="Road"
-              ></input>
-            </div> */}
+          <div className="mt-4 mb-7">
+            <button className="w-full inline-flex items-center justify-center px-4 py-2 bg-[#E54E3D] border border-transparent rounded-md font-semibold capitalize text-white hover:bg-red-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition">
+              ยืนยัน
+            </button>
+          </div>
+          <div className="mb-7">
+            <button className="w-full p-10 py-2 px-4 border-2 border-[#E54E3D] rounded text-[#E54E3D] hover:bg-[#E54E3D] hover:text-white transition duration-300">
+              ยกเลิก และ กลับสู่หน้าหลัก
+            </button>
           </div>
         </div>
       </div>
